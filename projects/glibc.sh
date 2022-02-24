41d0762945530918a88d40c6bb628ac505a0c401
Xeonacid <h.dwwwwww@gmail.com>
2022-02-24 18:35:48 +0800

    Update glibc.sh

--- projects/glibc.sh
+++ projects/glibc.sh
@@ -13,5 +13,6 @@ list_tags_h()
     grep -v "fedora" |
     grep -v "cvs" |
     grep -v -E "glibc-[0-9]*\.[0-9]*[\.-]" |
-    tac
+    tac |
+    sed -r 's/^glibc-([0-9]*)(\.[0-9]*)$/v\1 glibc-\1\2/'
 }
